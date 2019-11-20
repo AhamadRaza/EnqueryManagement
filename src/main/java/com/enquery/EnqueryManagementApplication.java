efd1017a969b09bbb163e2854cf8af2bcac77180
AhamadRaza <ahamadraza92786@gmail.com>
2019-11-21 01:10:23 +0530

    initial commit

---
+++ src/main/java/com/enquery/EnqueryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.enquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnqueryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnqueryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enquery/EnqueryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnqueryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
