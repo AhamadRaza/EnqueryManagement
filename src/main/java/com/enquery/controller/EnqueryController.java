49f106d650b0b1f9e801b42504d0f911c66c72ee
AhamadRaza <ahamadraza92786@gmail.com>
2019-11-21 15:13:54 +0530

    2 commit

--- src/main/java/com/enquery/controller/EnqueryController.java
+++ src/main/java/com/enquery/controller/EnqueryController.java
@@ -1,8 +1,14 @@
 package com.enquery.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class EnqueryController {
+
+    @GetMapping("/")
+    public String index(){
+        return "/index";
+    }
 
 }
---
+++ src/main/java/com/enquery/repository/EnquirySourceRepository.java
@@ -0,0 +1,8 @@
+package com.enquery.repository;
+
+import com.enquery.model.EnquirySource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnquirySourceRepository extends JpaRepository<EnquirySource , Long>{
+
+}
